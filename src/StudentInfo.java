8d52bd6880a633c05176d5c53753cd11cfff46fa
Nada7m <nadamo123123@gmail.com>
2025-11-12 13:59:35 +0300

    تجربة

--- src/StudentInfo.java
+++ src/StudentInfo.java
@@ -1,40 +1,58 @@
 
-/**
- *
- * @author Nadam
- */
+import javax.swing.JLabel;
+
+
 public class StudentInfo extends javax.swing.JFrame {
     public StudentInfo() {
-        initComponents();
+    initComponents();   
+
     }
+public StudentInfo(String name) {
+        initComponents(); 
+    Welcome.setText("Welcome, " + name + "!"); }
+
+    StudentInfo(JLabel Name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Welcome = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(628, 352));
+
+        Welcome.setText("jLabel1");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 475, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(195, 195, 195)
+                .addComponent(Welcome)
+                .addContainerGap(229, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 261, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(43, 43, 43)
+                .addComponent(Welcome)
+                .addContainerGap(198, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new StudentInfo().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new StudentInfo().setVisible(true);
         });
     }
+ 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Welcome;
     // End of variables declaration//GEN-END:variables
 }
